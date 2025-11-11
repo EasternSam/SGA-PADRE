e2f9b3de4b686ea8ecbc65487a809e7c2ad6c9bc
EasternSam <holasamueldiazpilier@gmail.com>
2025-11-11 12:01:19 -0400

    Ahora los módulos tienen precio

--- app/Livewire/Courses/Index.php
+++ app/Livewire/Courses/Index.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\Rule; // Para validación 'unique'
 use Carbon\Carbon; // Para manejar fechas/horas
 
-#[Layout('layouts.app')]
+#[Layout('Layouts.dashboard')]
 class Index extends Component
 {
     use WithPagination;
