c4e2c459f1c8197ba9a60fb540904fcc3ba16838
EasternSam <holasamueldiazpilier@gmail.com>
2026-01-15 14:52:30 -0400

    algo 2

--- resources/views/livewire/student-portal/my-payments.blade.php
+++ resources/views/livewire/student-portal/my-payments.blade.php
@@ -465,7 +465,7 @@
 
                 <div class="flex justify-end pt-4 mt-4 border-t">
                     <button @click="show = false; pdfUrl = ''" type="button" class="ml-3 inline-flex justify-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        Cerrar
+                        Cerrar por favor
                     </button>
                 </div>
             </div>
